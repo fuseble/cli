332cb8f353419d274c746d9f3c2b4faa2b800b7e
AlpoxDev <alpoxdev@gmail.com>
2022-09-19 01:08:52 +0900

    fix: .gitignore

--- bin/console.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chalk = require("chalk");
-console = Object.assign(Object.assign({}, console), { red: (...props) => console.log(chalk.red(props)), cyan: (...props) => console.log(chalk.cyan(props)) });
