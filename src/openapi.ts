317ed865165a1c7efb08794246dcb9bb4de07873
AlpoxDev <alpoxdev@gmail.com>
2022-09-19 01:18:09 +0900

    fix: openapi url

--- src/openapi.ts
+++ src/openapi.ts
@@ -18,7 +18,7 @@ apiClient.defaults.paramsSerializer = params => {
 }
 `;
 
-const createApiFunctionName = (method: string, url: string) => {
+const getApiFunctionName = (method: string, url: string) => {
   const splitUrl = url.split("/").map((part) => {
     if (part.includes("{") && part.includes("}")) {
       const param = part.replace("{", "").replace("}", "");
@@ -30,56 +30,77 @@ const createApiFunctionName = (method: string, url: string) => {
   return camelcase([method, ...splitUrl]);
 };
 
+const getApiUrlParams = (url: string) => {
+  return url
+    .split("/")
+    .map((part) => {
+      if (part.includes("{") && part.includes("}")) {
+        const param = part.replace("{", "").replace("}", "");
+        return param;
+      }
+      return undefined;
+    })
+    .filter(Boolean);
+};
+
+const getAPI = (method: string, url: string) => {
+  const functionName = getApiFunctionName(method, url);
+  const apiUrlParams = getApiUrlParams(url);
+  const apiUrl = url.replace(/{/g, "${params.").replace(/}/g, "}");
+
+  return { functionName, apiUrl };
+};
+
 const createGet = (url: string) => {
-  const functionName = createApiFunctionName("get", url);
+  const { functionName, apiUrl } = getAPI("get", url);
 
   return `
 export const ${functionName} = async (params: any) => {
-  const { data } = await apiClient.get('${url}', { params });
+  const { data } = await apiClient.get(\`${apiUrl}\`, { params });
   return data;
 };
   `;
 };
 
 export const createPost = (url: string) => {
-  const functionName = createApiFunctionName("post", url);
+  const { functionName, apiUrl } = getAPI("post", url);
 
   return `
 export const ${functionName} = async (params: any) => {
-  const { data } = await apiClient.post('${url}', params);
+  const { data } = await apiClient.post(\`${apiUrl}\`, params);
   return data;
 };
   `;
 };
 
 export const createPut = (url: string) => {
-  const functionName = createApiFunctionName("put", url);
+  const { functionName, apiUrl } = getAPI("put", url);
 
   return `
 export const ${functionName} = async (params: any) => {
-  const { data } = await apiClient.put('${url}', params);
+  const { data } = await apiClient.put(\`${apiUrl}\`, params);
   return data;
 };
   `;
 };
 
 export const createDelete = (url: string) => {
-  const functionName = createApiFunctionName("delete", url);
+  const { functionName, apiUrl } = getAPI("delete", url);
 
   return `
 export const ${functionName} = async (params: any) => {
-  const { data } = await apiClient.delete('${url}', { params });
+  const { data } = await apiClient.delete(\`${apiUrl}\`, { params });
   return data;
 };
   `;
 };
 
 export const createPatch = (url: string) => {
-  const functionName = createApiFunctionName("patch", url);
+  const { functionName, apiUrl } = getAPI("patch", url);
 
   return `
 export const ${functionName} = async (params: any) => {
-  const { data } = await apiClient.patch('${url}', params);
+  const { data } = await apiClient.patch(\`${apiUrl}\`, params);
   return data;
 };
   `;
